43ecec86de37eb3f617db26f5ab65a97e4d3a8df
My Name <myEmail@example.com>
2020-08-16 14:27:16 +0300

    last commit

--- src/js/parts/scrollMenu.js
+++ src/js/parts/scrollMenu.js
@@ -2,9 +2,9 @@ export default function scrollMenu() {
   $(".js__nav-menu").scroll(function () {
     let scrolledMenu = $(".js__nav-menu");
     let scrollWidth = $(".js__nav-menu").scrollLeft();
-    if (scrollWidth > 1 && scrollWidth < 320) {
+    if (scrollWidth > 1 && scrollWidth < 280) {
       scrolledMenu.addClass("scroll-start scroll-end");
-    } else if (scrollWidth > 300) {
+    } else if (scrollWidth > 280) {
       scrolledMenu.removeClass("scroll-end");
     } else if (scrollWidth == 0) {
       scrolledMenu.addClass("scroll-end");
