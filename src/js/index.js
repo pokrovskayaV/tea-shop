b75bda119095fb45ec3686cee6e9417addc49ffc
My Name <myEmail@example.com>
2020-11-06 21:49:08 +0300

    scroll

--- src/js/index.js
+++ src/js/index.js
@@ -7,5 +7,14 @@ $(document).ready(() => {
   footerMenu();
   itemBtn();
   initSliders();
-  initPopup(); 
+  initPopup();
+
+  /*$(".js-slider-tabs-content").on('afterChange', function(event, slick, currentSlide, nextSlide){
+
+    let slide = $(".js-slider-tab:not(.slick-current)");
+    let slideChild = $(".js-slider-tab:not(.slick-current) .items__product");
+    slide.css({'transform':'translate3d(0px,0px,0px)'});
+    slideChild.attr("aria-hidden", "false");
+    slideChild.attr("tabindex", "0");
+    })*/
 }); 
--- src/js/parts/sliders.js
+++ src/js/parts/sliders.js
@@ -9,29 +9,6 @@ let initSlidersMain = () => {
   });
 };
 
-let initSlidersItem = () => {
-  $(".js__slider-for").slick({
-    lazyLoad: 'ondemand',
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    infinite: true,
-    asNavFor: ".js__slider-nav"
-  });
-  $(".js__slider-nav").slick({
-    lazyLoad: 'ondemand',
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    asNavFor: ".js__slider-for",
-    dots: false,
-    focusOnSelect: true,
-    infinite: false,
-    arrows: false,
-    vertical: false,
-  });
-};
-
 let initSliderNews = () => {
   $(".js__slider-news").slick({
     lazyLoad: 'ondemand',
@@ -84,7 +61,125 @@ let initSliderNews = () => {
 };
 
 
+let initSlidersItem = () => {
+  $(".js__slider-for").slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    infinite: true,
+    asNavFor: ".js__slider-nav"
+  });
+  $(".js__slider-nav").slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: ".js__slider-for",
+    dots: false,
+    focusOnSelect: true,
+    infinite: false,
+    arrows: false,
+    vertical: false,
+  });
+};
+
+
+let initSlidersTabs = () => {
+   $(".js-slider-tabs-content").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    swipe: false,
+    fade: true,
+    
+  });
+
+ 
+  $(".js-slider-tabs-nav").slick({
+    lazyLoad: 'ondemand',
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    arrows: false,
+    infinite: false,
+    focusOnSelect: true,
+    asNavFor: ".js-slider-tabs-content",
+     
+    responsive: [
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 3.5,
+        }
+      },
+      {
+        breakpoint: 440,
+        settings: {
+          slidesToShow: 2.5,
+        }
+      },
+      {
+        breakpoint: 340,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+    ]
+     
+});
+};
+
+let initSliderTab = () => {
+  $(".js-slider-tab").slick({
+   lazyLoad: 'ondemand',
+   slidesToShow: 5,
+   slidesToScroll: 1,
+   arrows: false,
+   infinite: false,
+   cssEase:'ease-out',
+  
+   responsive: [
+     {
+       breakpoint: 1140,
+       settings: {
+         slidesToShow: 3.3,
+       }
+     },
+     {
+       breakpoint: 740,
+       settings: {
+         slidesToShow: 2.5,
+       }
+     },
+     {
+       breakpoint: 540,
+       settings: {
+         slidesToShow: 2,
+       }
+     },
+     {
+      breakpoint: 440,
+      settings: {
+        slidesToShow: 1.5,
+      }
+    },
+    {
+      breakpoint: 340,
+      settings: {
+        slidesToShow: 1.3,
+      }
+    },
+   ]
+ });
+
+
+};
+
+
 export default function initSliders() {
+  initSlidersTabs();
+  initSliderTab();
   initSlidersMain();
   initSlidersItem();
   initSliderNews();
